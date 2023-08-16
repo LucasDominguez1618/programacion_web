21de2ce11fd1b784e5518570c3af9078a67fd6c0
Lucas Dominguez <lucasdomin62@gmail.com>
2023-08-16 17:13:25 -0300

    actividades de objetos y arrays

--- trabajo/index.js
+++ trabajo/index.js
@@ -9,10 +9,87 @@
 //console.log(suma);
 //}
 //sumarPares(numbers);
-const palabrasA = ["hola", "seca", "mucha", "cosa"];
-const palabras = ["hola", "seca", "mucha", "coso"];
-function terminanEnA(words) {
-  const palabra = words.every((words) => words.endsWith("a"));
-  console.log(palabra);
+//const palabrasA = ["hola", "seca", "mucha", "cosa"];
+//const palabras = ["hola", "seca", "mucha", "coso"];
+//function terminanEnA(words) {
+//  const palabra = words.every((words) => words.endsWith("a"));
+//  console.log(palabra);
+//}
+//terminanEnA(palabrasA);
+//const numeros = [1, -2, -7, 8, -3];
+//function sortAbsoluteNumbers(numbers) {
+//  let numeros = [];
+//  numbers.map((item) => {
+//    const absoluteNumbers = Math.abs(item);
+//    numeros.push(absoluteNumbers);
+//  });
+//  return numeros.sort((a, b) => a - b);
+//}
+
+//console.log(sortAbsoluteNumbers(numeros));
+
+let words = [
+  "hola",
+  "tres",
+  "uno",
+  "Universidad",
+  "de",
+  "Buenos Aires",
+  "pepito",
+  "celular",
+];
+/*let word = "pepito";
+function buscaPalabras(words, word) {
+  const posicionpepito = words.indexOf(word);
+  console.log(posicionpepito);
+  const palabras = words.filter(function (word) {
+    return word.length > posicionpepito;
+  });
+  return palabras;
 }
-terminanEnA(palabrasA);
+console.log(buscaPalabras(words, word));
+
+
+function createObject(name, subs) {
+  hash = name.length * subs;
+  getStatus = function () {
+    console.log("el canal de", name, "tiene", subs);
+  };
+  const persona = {
+    hash,
+    name,
+    subs,
+    getStatus,
+  };
+  return persona;
+}
+const obj = createObject("Lukas", 1000);
+console.log(obj.getStatus());
+
+const person = {
+  name: "John",
+  age: 30,
+  hobbies: ["reading", "music", "sports"],
+};
+
+const { weight = 70, height = 180 } = person;
+console.log(weight, height);
+
+lo que se va a imprimir es weight y el height ya que es lo unico que esta en el console log
+
+
+const person = {
+  name: "John",
+  age: 30,
+  hobbies: ["reading", "music", "sports"],
+};
+
+const { age: personAge } = person;
+console.log(age);
+
+no se imprime nada porque age no existe afuera del Person
+
+
+
+3) el atajo que nos permite recuperar el valor de una propiedad de un objeto se le llama desestructuraciòn
+*/
